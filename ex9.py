35f0e6103d739807e55e7af0888c0b0c15fdd2d1
Stinstinstin <krikarlss@gmail.com>
2020-09-22 18:19:06 +0200

    Uppgift 9.2

--- ex9.py
+++ ex9.py
@@ -36,15 +36,15 @@ def game():
     for i in range(3):
         a = random.randint(1, 10)
         b = random.randint(1, 10)
-        answer = input(str(a) + "+" + str(b) + "=")
+        answer = input(f"{a}+{b}=")
         number = int(answer)
         if number == a + b:
             print("Rätt!")
             correct_answers += 1
         else:
             print(f"Fel... Det blir {a+b}")
-            print("---")
-    print("Du fick " + str(correct_answers) + " av 3 rätt.")
+    print("---")
+    print(f"Du fick {correct_answers} av 3 rätt.")
 
 
 if __name__ == '__main__':
