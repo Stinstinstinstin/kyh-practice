24a55c1ce46baa40ae26f52d933fa56dd848f22d
Stinstinstin <krikarlss@gmail.com>
2020-09-22 17:40:47 +0200

    Uppgift 1

---
+++ ex1.py
@@ -0,0 +1,15 @@
+"""
+20200922
+Kristin
+Uppgift 1
+Bygg ett program som skriver på skärmen:
+Erat namn och var ni växte upp.
+Har ni programmerat tidigare och i så fall i vilket språk / sedan när?
+Varför har ni sökt till KYH PvT?
+"""
+print("\n'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''")
+print("Hej! Kristin heter jag, född och uppvuxen i Göteborg.")
+print("Jag har programmerat lite i Python tidigare.")
+print("Jag sökte till KYH PvT för att programvarutestning lät väldigt roligt. :)")
+print("'''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''\n")
+
