10d34e307d8db2861558356c5c22bb2b469daedb
Stinstinstin <krikarlss@gmail.com>
2020-09-22 20:16:54 +0200

    Uppgift 10.2

---
+++ ex10_2.py
@@ -0,0 +1,17 @@
+"""
+20200922
+Kristin
+Uppgift 10
+
+Skriv flera program (t.ex ug10_1.py, ug10_2.py osv) som skriver ut:
+1) heltalen fr.o.m. 1 t.o.m. 10 i ökande ordning
+2) udda tal fr.o.m. 1 t.o.m. 49 i ökande ordning
+3) heltal fr.o.m. 10 t.o.m. 1 i minskande ordning
+4) summan av talen 7 t.o.m 1000
+5) produkten av talen 1 t.o.m 1000
+"""
+
+print("\nUdda heltal from 1 tom 49, ökande")
+
+for i in range(1, 49+1, 2):
+    print(i)
