5693520dcc5727bf3c4a95968650374ff1e4d203
Stinstinstin <krikarlss@gmail.com>
2020-09-22 17:43:11 +0200

    Uppgift 2

---
+++ ex2.py
@@ -0,0 +1,14 @@
+"""
+20200922
+Kristin
+Uppgift 2
+
+# Fråga användaren "What is your name?" och lagra svaret i en variabel med namnet "name"
+name = input("What is your name?")
+
+# Slå ihop textsträngen "Your name is: " och svaret som lagrats i variabeln "name", och skriv ut resultatet på skärmen
+print("Your name is: " + name)
+"""
+
+name = input("What is your name?")
+print(f"Your name is: {name}")
