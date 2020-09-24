aab58598e93aa9d3a8d830671e025bcf4f33d19b
Stinstinstin <krikarlss@gmail.com>
2020-09-24 11:57:26 +0200

    Uppgift 36 kollar poäng

--- uppgift36/main.py
+++ uppgift36/main.py
@@ -24,5 +24,15 @@ from pwstrength import compute_strength
 
 if __name__ == '__main__':
 
-    password = input("Ange lösenord: ")
-    compute_strength(password)
+    while True:
+        password = input("Ange lösenord: ")
+        res, passed = compute_strength(password)
+        if passed:
+            if res == 0:
+                print(f"Antal poäng: {res}. Svagt lösenord men godkänt.")
+            if res == 1:
+                print(f"Antal poäng: {res}. Bättre kan du. :)")
+            if res > 1:
+                print(f"Antal poäng: {res}. Bra lösenord! :)")
+            break
+        print("Ogiltligt lösenord! Försök igen :)")
