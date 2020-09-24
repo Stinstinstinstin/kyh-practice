0e598e7369d1a18e9ec2768def59310f37d1a7bd
Stinstinstin <krikarlss@gmail.com>
2020-09-24 08:44:16 +0200

    Uppgift 17 1. Visa lista 2. Lägg till uppgift

--- ex17.py
+++ ex17.py
@@ -20,21 +20,43 @@ Diskutera hur ni ska lösa uppgiften först tillsammans.
 """
 
 
-while True:
+def read_file(file_name):
+    f = open(file_name, 'r')
+    print("\n__This is your TODO list__")
+    for row in f:
+        print(row.rstrip())
+    f.close()
 
-    print("")
-    print("1. Lista TODO")
-    print("2. Lägg till en uppgift")
-    print("3. Ta bort uppgift")
-    print("4. Avbryt programmet")
-    choice = int(input("Ditt val: "))
 
-    if choice == 1:
-        pass
-    elif choice == 2:
-        pass
-    elif choice == 3:
-        pass
-    elif choice == 4:
-        print("\nProgrammet avslutas..")
-        quit()
+def write_to_file(file_name, content):
+    f = open(file_name, 'a')
+    f.write(content + "\n")
+    f.close()
+
+
+def main():
+
+    while True:
+
+        print("")
+        print("1. Lista TODO")
+        print("2. Lägg till en uppgift")
+        print("3. Ta bort uppgift")
+        print("4. Avbryt programmet")
+        choice = int(input("Ditt val: "))
+
+        if choice == 1:
+            read_file("ex17_todo.txt")
+
+        elif choice == 2:
+            new_task = input("Ny uppgift: ")
+            write_to_file("ex17_todo.txt", new_task)
+        elif choice == 3:
+            pass
+        elif choice == 4:
+            print("\nProgrammet avslutas..")
+            quit()
+
+
+if __name__ == '__main__':
+    main()
