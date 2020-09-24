0f148022f078a5605855a4318d9e8a757601f2fa
Stinstinstin <krikarlss@gmail.com>
2020-09-24 10:43:55 +0200

    Uppgift 36 any

--- uppgift36/pwstrength.py
+++ uppgift36/pwstrength.py
@@ -1,5 +1,4 @@
 """
-
   a) om lösenordet är mer än 10 tecken långt får det +1 poäng
   b) om lösenordet innehåller både siffror och bokstäver (det räcker med de engelska bokstäverna) så får det +1 poäng
   c) om lösenordet innehåller någon av symbolerna “#%&+_-” får det +1 poäng
@@ -8,7 +7,6 @@
 
 
 def compute_strength(pw):
-
     num_points = 0
 
     # a) om lösenordet är mer än 10 tecken långt får det +1 poäng
@@ -16,7 +14,8 @@ def compute_strength(pw):
         num_points += 1
         print("1 poäng - Längre än 10 tecken")
 
-    # b) om lösenordet innehåller både siffror och bokstäver (det räcker med de engelska bokstäverna) så får det +1 poäng
+    # b) om lösenordet innehåller både siffror och bokstäver (det räcker med de engelska bokstäverna)
+    # så får det +1 poäng
     if any(char.isdigit() for char in pw):
 
         if any(char.isalpha() for char in pw):
@@ -33,9 +32,10 @@ def compute_strength(pw):
     # d) om lösenordet innehåller något annat tecken än bokstäver, siffror och symbolerna i (3)
     # är det ogiltigt och får 0 poäng.
 
-    else:
+    if not (any(char.isdigit() for char in pw)
+            or any(char.isalpha() for char in pw)
+            or any(char in special_characters for char in pw)):
         print("Ogiltligt lösenord!")
         num_points = 0
-
 
     print(f"\nAntal poäng: {num_points}")
