8e2489a621e0cdb188d947920a6f6863080edbaa
Stinstinstin <krikarlss@gmail.com>
2020-09-25 20:51:24 +0200

    Uppgift 20 20.2 Increase growth speed

--- ex20.py
+++ ex20.py
@@ -142,7 +142,7 @@ def gameLoop():
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
             foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
-            length_of_snake += 1
+            length_of_snake += 2
 
         clock.tick(snake_speed)
 
