30e317bb562cc59077d6d0148dbbaead078cbc2d
Stinstinstin <krikarlss@gmail.com>
2020-09-29 15:08:09 +0200

    Uppgift 43 Cleanup

--- test_ex40.py
+++ test_ex40.py
@@ -1,4 +1,6 @@
 """
+20200929
+Kristin
 Uppgift 43
 Likt uppgift 42, skriv enhetstester för funktionerna från uppgift 40.
 """
@@ -6,44 +8,60 @@ Likt uppgift 42, skriv enhetstester för funktionerna från uppgift 40.
 from ex40 import reverse_text, check_if_upper, check_integer_relation
 
 
+# reverse_text()
 def test_reverse_text():
     assert reverse_text("abc") == "cba"
+
 
 def test_reverse_text_with_space():
     assert reverse_text("abc def") == "fed cba"
 
+
 def test_reverse_text_empty():
     assert reverse_text("") == ""
+
 
 def test_reverse_text_special_chars():
     assert reverse_text("abc&'") == "'&cba"
 
+
+# check_if_upper()
 def test_check_if_upper():
     assert check_if_upper("Abc") == 1
+
 
 def test_check_if_upper_more_than_one():
     assert check_if_upper("Abc DEF") == 4
 
+
 def test_check_if_upper_letters_numbers():
     assert check_if_upper("Abc12") == 1
+
 
 def test_check_if_upper_empty():
     assert check_if_upper("") == 0
 
+
+# check_integer_relation()
 def test_check_integer_relation():
-    assert check_integer_relation(2, 1, 3) == True
+    assert check_integer_relation(2, 1, 3) is True
+
 
 def test_check_integer_relation_wrong_params():
-    assert check_integer_relation(2, 3, 1) == False
+    assert check_integer_relation(2, 3, 1) is False
+
 
 def test_check_integer_relation_same_value():
-    assert check_integer_relation(1, 1, 1) == False
+    assert check_integer_relation(1, 1, 1) is False
+
 
 def test_check_integer_relation_same_value_not_true():
-    assert check_integer_relation(1, 1, 1) != True
+    assert check_integer_relation(1, 1, 1) is not True
+
 
 def test_check_integer_relation_same_values_zero():
-    assert check_integer_relation(0, 0, 0) == False
+    assert check_integer_relation(0, 0, 0) is False
+
 
 def test_check_integer_relation_negative_value():
-    assert check_integer_relation(-10, -20, 0) == True
+    assert check_integer_relation(-10, -20, 0) is True
